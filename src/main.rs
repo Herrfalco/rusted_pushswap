17f84c243030cf5742a8ff21845928a6f7434f17
Herrfalco <cadet.florian@gmail.com>
2021-12-24 17:46:54 +0100

    sort up to 4 values

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::collections::VecDeque;
 use std::{cmp, env, fmt, io, process};
 
+struct Solver {
+    ops: Vec<&'static str>,
+}
+
 struct Stacks {
     a: VecDeque<i32>,
     b: VecDeque<i32>,
@@ -25,6 +29,15 @@ impl fmt::Display for Stacks {
     }
 }
 
+impl fmt::Display for Solver {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for op in &self.ops {
+            writeln!(f, "{}", op)?;
+        }
+        Ok(())
+    }
+}
+
 impl Stacks {
     pub fn new() -> Result<Stacks, &'static str> {
         let stack_cap = env::args().skip(1).len();
@@ -39,32 +52,44 @@ impl Stacks {
                 Err(_) => return Err("wrong parameter"),
             }
         }
+        Stacks::check_dup(&stacks.a)?;
         Ok(stacks)
     }
 
     pub fn exec(&mut self, op: &str) -> Result<(), &'static str> {
         match op {
-            "sa\n" => self.a.swap(0, 1),
-            "sb\n" => self.b.swap(0, 1),
-            "ra\n" => self.a.rotate_left(1),
-            "rb\n" => self.b.rotate_left(1),
-            "rra\n" => self.a.rotate_right(1),
-            "rrb\n" => self.b.rotate_right(1),
-            "ss\n" => {
+            "sa" => self.a.swap(0, 1),
+            "sb" => self.b.swap(0, 1),
+            "ra" => self.a.rotate_left(1),
+            "rb" => self.b.rotate_left(1),
+            "rra" => self.a.rotate_right(1),
+            "rrb" => self.b.rotate_right(1),
+            "ss" => {
                 self.a.swap(0, 1);
                 self.b.swap(0, 1);
             }
-            "rr\n" => {
+            "rr" => {
                 self.a.rotate_left(1);
                 self.b.rotate_left(1);
             }
-            "rrr\n" => {
+            "rrr" => {
                 self.a.rotate_right(1);
                 self.b.rotate_right(1);
             }
-            "pa\n" => self.b.push_front(self.a.pop_front().unwrap()),
-            "pb\n" => self.a.push_front(self.b.pop_front().unwrap()),
+            "pa" => self.b.push_front(self.a.pop_front().unwrap()),
+            "pb" => self.a.push_front(self.b.pop_front().unwrap()),
             _ => return Err("unknown operation"),
+        }
+        Ok(())
+    }
+
+    fn check_dup(stack: &VecDeque<i32>) -> Result<(), &'static str> {
+        let mut copy: Vec<&i32> = stack.iter().collect();
+
+        copy.sort();
+        copy.dedup();
+        if copy.len() != stack.len() {
+            return Err("input duplicate");
         }
         Ok(())
     }
@@ -77,36 +102,99 @@ impl Stacks {
     }
 }
 
-/*
-fn get_rev_pair(stack: &VecDeque<i32>) -> Option<usize> {
-    for i in 0..stack.len() {
-        println!("({}, {})", stack[i], stack[(i + 1) % stack.len()]);
-        if stack[i] - stack[(i + 1) % stack.len()] == 1 {
-            return Some(i);
+impl Solver {
+    pub fn new() -> Solver {
+        Solver { ops: Vec::new() }
+    }
+
+    pub fn solve(&mut self, stacks: &mut Stacks) {
+        self.sort_til_4(stacks);
+    }
+
+    fn get_order(stack: &VecDeque<i32>) -> Vec<usize> {
+        let mut copy: Vec<(usize, &i32)> = stack.iter().enumerate().collect();
+        let mut result = vec![0; copy.len()];
+        let mut i = 0;
+
+        while copy.len() != 0 {
+            let (idx, _) =
+                copy.swap_remove(copy.iter().enumerate().min_by_key(|x| x.1 .1).unwrap().0);
+            result[idx] = i;
+            i += 1;
+        }
+        result
+    }
+
+    fn sort_til_4(&mut self, stacks: &mut Stacks) {
+        self.ops
+            .append(&mut match Solver::get_order(&stacks.a)[..] {
+                [1, 0] => vec!["sa"],
+                [0, 2, 1] => vec!["sa", "ra"],
+                [1, 0, 2] => vec!["sa"],
+                [1, 2, 0] => vec!["rra"],
+                [2, 0, 1] => vec!["ra"],
+                [2, 1, 0] => vec!["sa", "rra"],
+                [0, 1, 3, 2] => vec!["rra", "rra", "sa", "ra", "ra"],
+                [0, 2, 1, 3] => vec!["ra", "sa", "rra"],
+                [0, 2, 3, 1] => vec!["rra", "sa"],
+                [0, 3, 1, 2] => vec!["sa", "ra"],
+                [0, 3, 2, 1] => vec!["ra", "sa", "rra", "rra", "sa"],
+                [1, 0, 2, 3] => vec!["sa"],
+                [1, 0, 3, 2] => vec!["sa", "ra", "ra", "sa", "rra", "rra"],
+                [1, 2, 0, 3] => vec!["rra", "rra", "sa", "ra"],
+                [1, 2, 3, 0] => vec!["rra"],
+                [1, 3, 0, 2] => vec!["rra", "sa", "rra"],
+                [1, 3, 2, 0] => vec!["ra", "sa", "rra", "rra"],
+                [2, 0, 1, 3] => vec!["rra", "sa", "ra", "ra"],
+                [2, 0, 3, 1] => vec!["ra", "sa", "ra"],
+                [2, 1, 0, 3] => vec!["sa", "ra", "ra", "sa", "ra"],
+                [2, 1, 3, 0] => vec!["sa", "rra"],
+                [2, 3, 0, 1] => vec!["ra", "ra"],
+                [2, 3, 1, 0] => vec!["ra", "ra", "sa"],
+                [3, 0, 1, 2] => vec!["ra"],
+                [3, 0, 2, 1] => vec!["rra", "sa", "ra", "sa"],
+                [3, 1, 0, 2] => vec!["ra", "sa"],
+                [3, 1, 2, 0] => vec!["rra", "sa", "ra"],
+                [3, 2, 0, 1] => vec!["sa", "rra", "rra"],
+                [3, 2, 1, 0] => vec!["sa", "rra", "rra", "sa"],
+                _ => vec![],
+            });
+        for op in &self.ops {
+            stacks.exec(op);
         }
     }
-    None
 }
-*/
 
 fn error(msg: &str) {
     println!("Error: {}", msg);
     process::exit(1);
 }
 
+fn trim_ret(buff: &str) -> &str {
+    if buff.ends_with("\n") {
+        return &buff[..buff.len() - 1];
+    }
+    buff
+}
+
 fn main() {
     match Stacks::new() {
         Ok(mut stacks) => {
-            let mut buff = String::new();
+            let mut solver = Solver::new();
 
+            solver.solve(&mut stacks);
+            println!("{}", solver);
+            /*
+            let mut buff = String::new();
             loop {
                 println!("{}", &stacks);
                 io::stdin().read_line(&mut buff).unwrap();
-                if let Err(msg) = stacks.exec(&buff) {
+                if let Err(msg) = stacks.exec(trim_ret(&buff)) {
                     error(msg);
                 }
                 buff.clear();
             }
+            */
         }
         Err(msg) => error(msg),
     }
